71311133e8480a8d5d2797212a1e61dfedb20439
ken60 <kenken60826@gmail.com>
2016-06-30 23:54:00 +0900

    MobileInput

--- Trococo/Assets/Scripts/Player/Player.cs
+++ Trococo/Assets/Scripts/Player/Player.cs
@@ -1,44 +1,79 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    public Text m_Text;
     public float m_JumpForce;
-
-    public  bool m_isGrounded;
+    public float m_RayDist = 0.5f;
+    public bool m_isGrounded = false;
 
     private Rigidbody m_Rigidbody;
     private Touch m_Touch;
+    private Ray ray;
+    private RaycastHit hit;
 
     void Start()
     {
         m_Rigidbody = GetComponent<Rigidbody>();
-        m_isGrounded = false;
-
+        m_Text.text = "none";
     }
 
     void Update()
     {
-#if UNITY_EDITOR
-        if (Input.GetKeyDown(KeyCode.Space) && m_isGrounded)
+        // Debug.DrawRay(transform.position, Vector3.down * m_RayDist, Color.red);
+
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, m_RayDist))
+        {
+            if (hit.collider.tag == "Ground")
+                m_isGrounded = true;
+        }
+
+        //タップでジャンプ
+        if (MobileInput.GetFlick() == MobileInput.TouchDir.UP && m_isGrounded)
         {
             m_Rigidbody.AddForce(Vector3.up * m_JumpForce);
             m_isGrounded = false;
         }
 
-
-#else
-        if (Input.touchCount > 0 && m_isGrounded)
+        //下方向フリックでしゃがむ
+        if (MobileInput.GetFlick() == MobileInput.TouchDir.DOWN && m_isGrounded)
         {
-            m_Rigidbody.AddForce(Vector3.up * m_JumpForce);
-        m_isGrounded = false;
+
         }
-#endif
-    }
+        
 
-    void OnCollisionEnter(Collision col)
-    {
-        m_isGrounded = true;
+        switch (MobileInput.GetFlick())
+        {
+            case MobileInput.TouchDir.NONE:
+                //m_Text.text = "なし";
+                break;
 
+            case MobileInput.TouchDir.UP:
+                //上フリックされた時の処理
+                m_Text.text = "上フリック";
+                break;
+
+            case MobileInput.TouchDir.DOWN:
+                //下フリックされた時の処理
+                m_Text.text = "下フリック";
+                break;
+
+            case MobileInput.TouchDir.RIGHT:
+                //右フリックされた時の処理
+                m_Text.text = "右フリック";
+                break;
+
+            case MobileInput.TouchDir.LEFT:
+                //左フリックされた時の処理
+                m_Text.text = "左フリック";
+                break;
+
+            case MobileInput.TouchDir.TOUCH:
+                //タッチされた時の処理
+                m_Text.text = "タッチ";
+                break;
+        }
     }
 }
---
+++ Trococo/Assets/Scripts/System/MobileInput.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+public class MobileInput : MonoBehaviour
+{
+    private static Vector2 m_StartPos;
+    private static Vector2 m_EndPos;
+    private static string m_Direction;
+    private static TouchDir m_TouchDir;
+
+    public enum TouchDir
+    {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT,
+        TOUCH,
+        NONE
+    }
+
+
+    public static TouchDir GetFlick()
+    {
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0))
+        {
+            m_StartPos = Input.mousePosition;
+        }
+
+        else if (Input.GetMouseButtonUp(0))
+        {
+            m_EndPos = Input.mousePosition;
+            GetDirection();
+        }
+        else
+        {
+            m_TouchDir = TouchDir.NONE;
+        }
+#elif UNITY_IOS || UNITY_ANDROID
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            switch (touch.phase)
+            {
+                //最初にタッチされた座標を取得
+                case TouchPhase.Began:
+                    m_StartPos = touch.position;
+                    break;
+
+                //指を離した座標を取得
+                case TouchPhase.Ended:
+                    m_EndPos = touch.position;
+                    GetDirection();
+                    break;
+            }
+        }
+        else
+        {
+            m_TouchDir = TouchDir.NONE;
+        }
+#endif
+        return m_TouchDir;
+    }
+
+
+    static void GetDirection()
+    {
+        Vector2 direction = m_EndPos - m_StartPos;
+
+        if (Mathf.Abs(direction.y) < Mathf.Abs(direction.x))
+        {
+            if (30 < direction.x)
+            {
+                //右向きにフリック
+                m_TouchDir = TouchDir.RIGHT;
+            }
+            else if (-30 > direction.x)
+            {
+                //左向きにフリック
+                m_TouchDir = TouchDir.LEFT;
+            }
+        }
+        else if (Mathf.Abs(direction.x) < Mathf.Abs(direction.y))
+        {
+            if (30 < direction.y)
+            {
+                //上向きにフリック
+                m_TouchDir = TouchDir.UP;
+            }
+            else if (-30 > direction.y)
+            {
+                //下向きのフリック
+                m_TouchDir = TouchDir.DOWN;
+            }
+        }
+        else
+        {
+            //タッチを検出
+            m_TouchDir = TouchDir.TOUCH;
+        }
+    }
+}

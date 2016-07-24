4e6d71a2b60ab3c5aa38c5fc8064b3cd14c23397
ken60 <kenken60826@gmail.com>
2016-07-24 23:30:16 +0900

    fix

--- Assets/Scripts/System/GameSceneManager.cs
+++ Assets/Scripts/System/GameSceneManager.cs
@@ -1,24 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameSceneManager : SingletonMonoBehaviour<GameSceneManager>
+public class GameSceneManager : MonoBehaviour
 {
-    private bool m_GameStart = false;
-    private bool m_HawToPlay = false;
-    private bool m_isGamePlaying = false;
-    private bool m_isGameOver = false;
-    private bool m_isCollider = true;
-
-    public void InitGame()
+    public enum eGameScene
     {
+        LoadTitle = 0,
+        Title,
+        LoadGame,
+        StartCount,
+        Play,
+        GameOver,
+        End
+    }
+
+    private static bool m_GameStart = false;
+    private static bool m_HawToPlay = false;
+    private static bool m_isGamePlaying = false;
+    private static bool m_isGameOver = false;
+    private static bool m_isCollider = true;    //Debug only
+    private static eGameScene m_Scene;
+
+    public static void InitGame()
+    {
+        m_Scene = eGameScene.LoadTitle;
         m_GameStart = false;
         m_HawToPlay = false;
         m_isGamePlaying = false;
         m_isGameOver = false;
+        m_isCollider = true;    //Debug only
     }
-    
-    
-    public bool isGameOver
+
+    public static eGameScene scene
+    {
+        get
+        {
+            return m_Scene;
+        }
+        set
+        {
+            m_Scene = value;
+        }
+    }
+
+
+    public static bool isGameOver
     {
         get
         {
@@ -30,7 +56,7 @@ public class GameSceneManager : SingletonMonoBehaviour<GameSceneManager>
         }
     }
 
-    public bool isGamePlaying
+    public static bool isGamePlaying
     {
         get
         {
@@ -42,7 +68,8 @@ public class GameSceneManager : SingletonMonoBehaviour<GameSceneManager>
         }
     }
 
-    public bool colliderEnabled
+    //Debug only
+    public static bool colliderEnabled
     {
         get
         {

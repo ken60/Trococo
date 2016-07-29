758743788603e9d3371a22927d6fb73384beaf21
ken60 <kenken60826@gmail.com>
2016-07-29 16:01:56 +0900

    update

---
+++ Assets/Scripts/System/SceneChangeFade.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneChangeFade : MonoBehaviour
+{
+    [Range(0.0f, 1.0f), SerializeField]
+    private float m_Alpha;
+
+    private Color m_Color;
+    private Image m_Image;
+
+    void Start()
+    {
+        m_Image = GetComponent<Image>();
+        m_Color = m_Image.color;
+    }
+
+    void Update()
+    {
+        m_Color.a = m_Alpha;
+        m_Image.color = m_Color;
+    }
+
+
+    public void FadeOut()
+    {
+        if (0.0f < m_Alpha)
+        {
+            m_Alpha -= Time.deltaTime;
+        }
+    }
+
+    public void FadeIn()
+    {
+        if (m_Alpha < 1.0f)
+        {
+            m_Alpha += Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Sort_GUI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Sort_GUI : MonoBehaviour
+{
+    [SerializeField, Tooltip("描画順番。大きいほど手前に")]
+    private int index = 0;
+
+    void Update()
+    {
+        transform.SetSiblingIndex(index);
+    }
+}
